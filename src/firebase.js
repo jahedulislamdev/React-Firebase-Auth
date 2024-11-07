e618625c6d3f85746ecb1cf8a696c7ef59b11eac
JISHAN <jahedulislamdev@gmail.com>
2024-11-07 20:21:05 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCTJPLMs2EpaSMMjFwBmHntHeytm8Pwkhw",
+    authDomain: "simle-firebase-84a45.firebaseapp.com",
+    projectId: "simle-firebase-84a45",
+    storageBucket: "simle-firebase-84a45.firebasestorage.app",
+    messagingSenderId: "507986017300",
+    appId: "1:507986017300:web:ed87bb5e301f5ee5927b01",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
